d81c8bb2e1a1b6346128e8c85a11fcc8b5d7e716
Pooyan J <pooyanjavidi@gmail.com>
2025-05-29 06:28:23 +0330

    Initial Commit

---
+++ PaginationTrainingTests/PaginationTrainingTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaginationTrainingTests.swift
+//  PaginationTrainingTests
+//
+//  Created by Pooyan J on 5/29/25.
+//
+
+import Testing
+@testable import PaginationTraining
+
+struct PaginationTrainingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
